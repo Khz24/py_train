9147b9c16c100b50fb8db1cccd4eb6c00d79b2ff
admin <Khalil.Zeiaei@Gmail.Com>
2024-01-07 15:55:41 +0330

    create tables

--- crud/models.py
+++ crud/models.py
@@ -5,7 +5,6 @@ from django.db import models
 
 class Job(models.Model):
     name = models.CharField(max_length=30)
-    email = models.EmailField()
     create = models.DateTimeField(auto_now=True)
     update = models.DateTimeField(auto_now_add=True)
 
@@ -15,6 +14,8 @@ class Job(models.Model):
 
 class Person(models.Model):
     name = models.CharField(max_length=30)
+    mobile = models.CharField(max_length=11)
+    work_phone = models.CharField(max_length=11)
     jobCode = models.ForeignKey(Job, on_delete=models.CASCADE, null=False, blank=False, related_name='PersonJob')
     create = models.DateTimeField(auto_now=True)
     update = models.DateTimeField(auto_now_add=True)
