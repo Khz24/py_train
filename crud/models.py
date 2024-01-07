bb0430e85ff9d05b13c320f7143e5338d9eb5662
admin <Khalil.Zeiaei@Gmail.Com>
2024-01-07 14:51:00 +0330

    add- models.py

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Job(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    create = models.DateTimeField(auto_now=True)
+    update = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    jobCode = models.ForeignKey(Job, on_delete=models.CASCADE, null=False, blank=False, related_name='PersonJob')
+    create = models.DateTimeField(auto_now=True)
+    update = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
